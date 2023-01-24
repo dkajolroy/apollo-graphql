7f979eba9360f78d3b3f21ee46fe5b873d94fa35
kajolroy <shrikajol@gmail.com>
2023-01-25 00:51:35 +0600

    get data query done

--- schema/TypeDefs.js
+++ schema/TypeDefs.js
@@ -1,12 +1,14 @@
 const { gql } = require("apollo-server");
 
 exports.typeDefs = gql`
-  type User {
+  type UserTypeSchema {
     name: String!
     username: String!
     age: Int!
+    id: Int
   }
+
   type Query {
-    users: [User!]!
+    users: [UserTypeSchema]
   }
 `;
